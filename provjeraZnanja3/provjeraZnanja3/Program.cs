4d34a469df2a63c8004a95bd3afab4835b869bfd
Non't <90344104+Shef905015@users.noreply.github.com>
2023-11-15 08:44:56 +0100

    Add files via upload

---
+++ provjeraZnanja3/provjeraZnanja3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provjeraZnanja3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 0;
+            int rezultat = 0;
+            Console.Write("Unesite željeni broj Fibonaccijevog niza: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            List<int> privremeni = new List<int>();
+
+            privremeni.Add(0);
+            privremeni.Add(1);
+            for (int i = 0; i < n; i++)
+            {
+                rezultat = rezultat + privremeni[i];
+                privremeni.Add(rezultat);
+
+            }
+
+
+            Console.WriteLine(n + ". broj Fibonaccijevog niza je " + rezultat);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
