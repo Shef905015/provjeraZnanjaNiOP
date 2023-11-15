2726d94d996605ca188d1ece83cda6afea1cc986
Non't <90344104+Shef905015@users.noreply.github.com>
2023-11-15 08:21:03 +0100

    Add files via upload

---
+++ provjeraZnanja1/provjeraZnanja1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provjeraZnanja1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string rijec;
+
+            Console.WriteLine("Unesite neki niz znakova: ");
+            rijec = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine(rijec.Replace(" ", "_"));
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
